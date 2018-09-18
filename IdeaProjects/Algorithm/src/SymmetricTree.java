98e6bfa63cd2b4c4b68addcf6385f17e2aea4f25
chen7weijie <chen7weijie@163.com>
2018-09-18 11:33:39 +0800

    2018/9/18

---
+++ IdeaProjects/Algorithm/src/SymmetricTree.java
@@ -0,0 +1,13 @@
+import DataStructure.TreeNode;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return root==null||helper(root.left,root.right);
+    }
+
+    public boolean helper(TreeNode left,TreeNode right){
+        if(left==null||right==null)return left==right;
+        if(left.val!=right.val)return false;
+        return helper(left.left,right.right)&&helper(left.right,right.left);
+    }
+}
